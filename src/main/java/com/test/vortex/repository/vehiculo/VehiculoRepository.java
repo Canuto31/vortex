6d931f050492315cce666c6859e250e005a837e8
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-05 12:12:07 -0500

    Create the generical methods for the VehiculoRepository interface

--- src/main/java/com/test/vortex/repository/vehiculo/VehiculoRepository.java
+++ src/main/java/com/test/vortex/repository/vehiculo/VehiculoRepository.java
@@ -1,4 +1,14 @@
 package com.test.vortex.repository.vehiculo;
 
+import com.test.vortex.model.dto.VehicleDto;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface VehiculoRepository {
+
+    List<VehicleDto> getAll();
+    Optional<VehicleDto> getVehicleById(int idVehicle);
+    VehicleDto save(VehicleDto vehicleDto);
+    void delete(int idVehicle);
 }
