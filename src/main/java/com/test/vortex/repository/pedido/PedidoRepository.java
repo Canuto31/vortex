381165da709f27a025c8b9255f3a9eaea25745a5
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-05 13:10:59 -0500

    Modify the repository interface for the PedidoRepository and VehiculoRepository

--- src/main/java/com/test/vortex/repository/pedido/PedidoRepository.java
+++ src/main/java/com/test/vortex/repository/pedido/PedidoRepository.java
@@ -1,6 +1,7 @@
 package com.test.vortex.repository.pedido;
 
 import com.test.vortex.model.dto.OrderDto;
+import com.test.vortex.model.entity.Pedido;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,6 +9,8 @@ import java.util.Optional;
 public interface PedidoRepository {
 
     List<OrderDto> getAll();
+
+    Optional<List<Pedido>> getOrderByIdDriver(int idDriver);
     Optional<OrderDto> getOrderById(int idOrder);
     OrderDto save(OrderDto orderDto);
     void delete(int idOrder);
--- src/main/java/com/test/vortex/repository/vehiculo/VehiculoRepository.java
+++ src/main/java/com/test/vortex/repository/vehiculo/VehiculoRepository.java
@@ -1,6 +1,7 @@
 package com.test.vortex.repository.vehiculo;
 
 import com.test.vortex.model.dto.VehicleDto;
+import com.test.vortex.model.entity.Pedido;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,6 +9,8 @@ import java.util.Optional;
 public interface VehiculoRepository {
 
     List<VehicleDto> getAll();
+
+    Optional<List<Pedido>> getOrderByIdDriver(int idDriver);
     Optional<VehicleDto> getVehicleById(int idVehicle);
     VehicleDto save(VehicleDto vehicleDto);
     void delete(int idVehicle);
