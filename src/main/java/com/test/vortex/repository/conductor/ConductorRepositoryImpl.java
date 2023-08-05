5ebf1981b6645aa3497b036baf297fa41b25d66a
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-05 12:52:07 -0500

    Add the Repository annotation, add the implementation of the ConductorRepository and the ConductorCrudRepository. Currently without logic into the methods

--- src/main/java/com/test/vortex/repository/conductor/ConductorRepositoryImpl.java
+++ src/main/java/com/test/vortex/repository/conductor/ConductorRepositoryImpl.java
@@ -1,4 +1,38 @@
 package com.test.vortex.repository.conductor;
 
-public class ConductorRepositoryImpl {
+import com.test.vortex.mapper.ConductorMapper;
+import com.test.vortex.model.dto.DriverDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ConductorRepositoryImpl implements ConductorRepository{
+
+    @Autowired
+    private ConductorCrudRepository crudRepository;
+    @Autowired
+    private ConductorMapper mapper;
+
+    @Override
+    public List<DriverDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<DriverDto> getDriverById(int idDriver) {
+        return Optional.empty();
+    }
+
+    @Override
+    public DriverDto save(DriverDto driverDto) {
+        return null;
+    }
+
+    @Override
+    public void delete(int idDriver) {
+
+    }
 }
