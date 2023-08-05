ee1c1b7f586ebef1f051540b755106556f0f827e
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-05 11:18:23 -0500

    Creation of the three necessaries entities for the project

--- src/main/java/com/test/vortex/model/entity/Conductor.java
+++ src/main/java/com/test/vortex/model/entity/Conductor.java
@@ -1,4 +1,32 @@
 package com.test.vortex.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "CONDUCTOR")
+@Data
 public class Conductor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String identificacion;
+
+    private String apellido;
+
+    private String nombre;
+
+    private String telefono;
+
+    private String direccion;
+
+    @OneToMany(mappedBy = "conductor")
+    private List<Pedido> pedidos;
+
+    @OneToMany(mappedBy = "conductor")
+    private List<Vehiculo> vehiculos;
 }
--- src/main/java/com/test/vortex/model/entity/Pedido.java
+++ src/main/java/com/test/vortex/model/entity/Pedido.java
@@ -1,4 +1,26 @@
 package com.test.vortex.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "PEDIDO")
+@Data
 public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "tipo_pedido")
+    private String tipoPedido;
+
+    private String direccion;
+
+    @Column(name = "conductor_id")
+    private Integer conductorId;
+
+    @ManyToOne
+    @JoinColumn(name = "conductor_id")
+    private Conductor conductor;
 }
