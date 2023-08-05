e727531628799307d660ca11623e14f649d64993
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-05 12:54:53 -0500

    Add the Repository annotation, add the implementation of the VehiculoRepository and the VehiculoCrudRepository. Currently without logic into the methods

--- src/main/java/com/test/vortex/repository/vehiculo/VehiculoRepositoryImpl.java
+++ src/main/java/com/test/vortex/repository/vehiculo/VehiculoRepositoryImpl.java
@@ -1,4 +1,38 @@
 package com.test.vortex.repository.vehiculo;
 
-public class VehiculoRepositoryImpl {
+import com.test.vortex.mapper.VehiculoMapper;
+import com.test.vortex.model.dto.VehicleDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class VehiculoRepositoryImpl implements VehiculoRepository{
+
+    @Autowired
+    private VehiculoCrudRepository crudRepository;
+    @Autowired
+    private VehiculoMapper mapper;
+
+    @Override
+    public List<VehicleDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<VehicleDto> getVehicleById(int idVehicle) {
+        return Optional.empty();
+    }
+
+    @Override
+    public VehicleDto save(VehicleDto vehicleDto) {
+        return null;
+    }
+
+    @Override
+    public void delete(int idVehicle) {
+
+    }
 }
