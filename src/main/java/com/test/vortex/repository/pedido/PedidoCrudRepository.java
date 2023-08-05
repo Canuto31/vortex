41497f2f59729eee9eaef122d4ad8c22d7bf4846
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-05 12:00:25 -0500

    Into the PedidoCrudRepository add the extend of Crud Repository

--- src/main/java/com/test/vortex/repository/pedido/PedidoCrudRepository.java
+++ src/main/java/com/test/vortex/repository/pedido/PedidoCrudRepository.java
@@ -1,4 +1,11 @@
 package com.test.vortex.repository.pedido;
 
-public interface PedidoCrudRepository {
+import com.test.vortex.model.entity.Pedido;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PedidoCrudRepository extends CrudRepository<Pedido, Integer> {
+
+    List<Pedido> findByIdDriverOrderByFirstNameAsc(int idDriver);
 }
