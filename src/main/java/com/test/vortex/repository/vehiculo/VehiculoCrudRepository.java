df8454955835bfeece380d82c90f55a59b55d6b8
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-05 12:01:30 -0500

    Into the VehiculoCrudRepository add the extend of Crud Repository

--- src/main/java/com/test/vortex/repository/vehiculo/VehiculoCrudRepository.java
+++ src/main/java/com/test/vortex/repository/vehiculo/VehiculoCrudRepository.java
@@ -1,4 +1,11 @@
 package com.test.vortex.repository.vehiculo;
 
-public interface VehiculoCrudRepository {
+import com.test.vortex.model.entity.Vehiculo;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface VehiculoCrudRepository extends CrudRepository<Vehiculo, Integer> {
+
+    List<Vehiculo> findByIdDriverOrderByFirstNameAsc(int idDriver);
 }
