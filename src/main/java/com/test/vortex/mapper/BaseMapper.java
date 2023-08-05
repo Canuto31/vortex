654288e256346d61b3c1251181ceba19902c23e3
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-05 11:29:11 -0500

    Creation of the generical methods for the mappers

--- src/main/java/com/test/vortex/mapper/BaseMapper.java
+++ src/main/java/com/test/vortex/mapper/BaseMapper.java
@@ -1,4 +1,14 @@
 package com.test.vortex.mapper;
 
-public interface BaseMapper {
+import java.util.List;
+
+public interface BaseMapper<D, E> {
+
+    public D entityToDto(E entity);
+
+    public E dtoToEntity(D dto);
+
+    public List<D> entitiesToDtos(List<E> entities);
+
+    public List<E> DtosToEntities(List<D> dtos);
 }
