433e47d986f33ecd157710aeafbcd19fe02869d5
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-05 12:53:24 -0500

    Add the Repository annotation, add the implementation of the PedidoRepository and the PedidoCrudRepository. Currently without logic into the methods

--- src/main/java/com/test/vortex/repository/pedido/PedidoRepositoryImpl.java
+++ src/main/java/com/test/vortex/repository/pedido/PedidoRepositoryImpl.java
@@ -1,4 +1,38 @@
 package com.test.vortex.repository.pedido;
 
-public class PedidoRepositoryImpl {
+import com.test.vortex.mapper.PedidoMapper;
+import com.test.vortex.model.dto.OrderDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class PedidoRepositoryImpl implements PedidoRepository{
+
+    @Autowired
+    private PedidoCrudRepository crudRepository;
+    @Autowired
+    private PedidoMapper mapper;
+
+    @Override
+    public List<OrderDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<OrderDto> getOrderById(int idOrder) {
+        return Optional.empty();
+    }
+
+    @Override
+    public OrderDto save(OrderDto orderDto) {
+        return null;
+    }
+
+    @Override
+    public void delete(int idOrder) {
+
+    }
 }
