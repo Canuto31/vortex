d51e30c1593b15603f5948a53201133c2c440d9c
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-08 06:43:13 -0500

    Creation of the methods for VehiculoService and implementation without logic into the VehiculoServiceImpl

--- src/main/java/com/test/vortex/service/vehiculo/VehiculoService.java
+++ src/main/java/com/test/vortex/service/vehiculo/VehiculoService.java
@@ -1,4 +1,15 @@
 package com.test.vortex.service.vehiculo;
 
+import com.test.vortex.model.dto.VehicleDto;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface VehiculoService {
+
+    List<VehicleDto> getAll();
+    Optional<List<VehicleDto>> getVehicleByIdDriver(int idVehicle);
+    Optional<VehicleDto> getVehicleById(int idVehicle);
+    VehicleDto save(VehicleDto vehicleDto);
+    Boolean delete(int idVehicle);
 }
--- src/main/java/com/test/vortex/service/vehiculo/VehiculoServiceImpl.java
+++ src/main/java/com/test/vortex/service/vehiculo/VehiculoServiceImpl.java
@@ -1,4 +1,35 @@
 package com.test.vortex.service.vehiculo;
 
-public class VehiculoServiceImpl {
+import com.test.vortex.model.dto.VehicleDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VehiculoServiceImpl implements VehiculoService{
+    @Override
+    public List<VehicleDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<List<VehicleDto>> getVehicleByIdDriver(int idVehicle) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<VehicleDto> getVehicleById(int idVehicle) {
+        return Optional.empty();
+    }
+
+    @Override
+    public VehicleDto save(VehicleDto vehicleDto) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(int idVehicle) {
+        return null;
+    }
 }
