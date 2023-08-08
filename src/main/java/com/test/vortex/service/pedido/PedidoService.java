c40e53557886000e3ae21fe49d079518befcc3c5
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-08 06:41:01 -0500

    Creation of the methods for PedidoService and implementation without logic into the PedidoServiceImpl

--- src/main/java/com/test/vortex/service/pedido/PedidoService.java
+++ src/main/java/com/test/vortex/service/pedido/PedidoService.java
@@ -1,4 +1,15 @@
 package com.test.vortex.service.pedido;
 
+import com.test.vortex.model.dto.OrderDto;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface PedidoService {
+
+    List<OrderDto> getAll();
+    Optional<List<OrderDto>> getOrderByIdDriver(int idOrder);
+    Optional<OrderDto> getOrderById(int irOrder);
+    OrderDto save(OrderDto orderDto);
+    Boolean delete(int idOrder);
 }
--- src/main/java/com/test/vortex/service/pedido/PedidoServiceImpl.java
+++ src/main/java/com/test/vortex/service/pedido/PedidoServiceImpl.java
@@ -1,4 +1,36 @@
 package com.test.vortex.service.pedido;
 
-public class PedidoServiceImpl {
+import com.test.vortex.model.dto.OrderDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PedidoServiceImpl implements PedidoService{
+
+    @Override
+    public List<OrderDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<List<OrderDto>> getOrderByIdDriver(int idOrder) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<OrderDto> getOrderById(int irOrder) {
+        return Optional.empty();
+    }
+
+    @Override
+    public OrderDto save(OrderDto orderDto) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(int idOrder) {
+        return null;
+    }
 }
