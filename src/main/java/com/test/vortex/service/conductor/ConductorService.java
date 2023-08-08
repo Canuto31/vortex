f478a05e391c29c818680f09a865f14c3785dc5c
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-07 20:08:48 -0500

    Creation of the methods for ConductorService and implementation without logic into the ConductorServiceImpl

--- src/main/java/com/test/vortex/service/conductor/ConductorService.java
+++ src/main/java/com/test/vortex/service/conductor/ConductorService.java
@@ -1,4 +1,15 @@
 package com.test.vortex.service.conductor;
 
+import com.test.vortex.model.dto.DriverDto;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ConductorService {
+
+    List<DriverDto> getAll();
+    Optional<DriverDto> getDriverByIdentification(int identification);
+    Optional<DriverDto> getDriverById(int idDriver);
+    DriverDto save(DriverDto driverDto);
+    Boolean delete(int idDriver);
 }
--- src/main/java/com/test/vortex/service/conductor/ConductorServiceImpl.java
+++ src/main/java/com/test/vortex/service/conductor/ConductorServiceImpl.java
@@ -1,4 +1,35 @@
 package com.test.vortex.service.conductor;
 
-public class ConductorServiceImpl {
+import com.test.vortex.model.dto.DriverDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ConductorServiceImpl implements ConductorService{
+    @Override
+    public List<DriverDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<DriverDto> getDriverByIdentification(int identification) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<DriverDto> getDriverById(int idDriver) {
+        return Optional.empty();
+    }
+
+    @Override
+    public DriverDto save(DriverDto driverDto) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(int idDriver) {
+        return null;
+    }
 }
