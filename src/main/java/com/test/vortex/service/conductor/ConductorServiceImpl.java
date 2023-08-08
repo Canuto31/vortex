4dba63d8eb979d4cc7c6078bebc545d8279f8390
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-08 07:52:10 -0500

    Add the logic into the methods of the ConductorServiceImpl

--- src/main/java/com/test/vortex/service/conductor/ConductorServiceImpl.java
+++ src/main/java/com/test/vortex/service/conductor/ConductorServiceImpl.java
@@ -1,6 +1,8 @@
 package com.test.vortex.service.conductor;
 
 import com.test.vortex.model.dto.DriverDto;
+import com.test.vortex.repository.conductor.ConductorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,28 +10,34 @@ import java.util.Optional;
 
 @Service
 public class ConductorServiceImpl implements ConductorService{
+
+    @Autowired
+    private ConductorRepository repository;
     @Override
     public List<DriverDto> getAll() {
-        return null;
+        return repository.getAll();
     }
 
     @Override
     public Optional<DriverDto> getDriverByIdentification(String identification) {
-        return Optional.empty();
+        return repository.getDriverByIdentification(identification);
     }
 
     @Override
     public Optional<DriverDto> getDriverById(int idDriver) {
-        return Optional.empty();
+        return repository.getDriverById(idDriver);
     }
 
     @Override
     public DriverDto save(DriverDto driverDto) {
-        return null;
+        return repository.save(driverDto);
     }
 
     @Override
     public Boolean delete(int idDriver) {
-        return null;
+        return getDriverById(idDriver).map(driver -> {
+            repository.delete(idDriver);
+            return true;
+        }).orElse(false);
     }
 }
